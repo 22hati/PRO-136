d049e9bc5674db6751abb3832b82e3c4e27788ef
22hati <67973707+22hati@users.noreply.github.com>
2022-12-03 10:08:04 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from final_data import data
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return jsonify({
+        "data": data,
+        "message": "success",
+    }),200
+
+if __name__ == "__main__":
+    app.run()
